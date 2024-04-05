ea9fd3b614cf9f36d5f0b343c47eb9e352a4a02c
Dmitrii <stakheev12@mail.ru>
2024-04-05 21:50:26 +0500

    Revert "Add project structure"
    This reverts commit df4d44f20ad575cf64e704d5debe23576a166d49.

--- app/api/routers.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from app.core.conf import settings
-from app.api.v1.Tests import tests_app
-
-v1 = APIRouter(prefix=settings.API_V1_STR)
-
-v1.include_router(tests_app, prefix='/tests', tags=['Tests'])
